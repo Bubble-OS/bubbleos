ab5dcd2ddc7db044a2e849e26124cd9d295a7d7a
Arnav Thorat <arnavthorat78@gmail.com>
2022-07-04 19:42:40 +1000

    Added help

--- index.js
+++ index.js
@@ -12,31 +12,33 @@ const taskkill = require("./src/commands/taskkill");
 require("./src/intro");
 
 while (true) {
-  const command = prompt();
+    const command = prompt();
 
-  error(command);
+    error(command);
 
-  if (command.isExit) {
-    require("./src/exit");
-  } else if (command.command.startsWith("cd")) {
-    const directory = command.command.replace("cd ", "");
+    if (command.isExit) {
+        require("./src/exit");
+    } else if (command.isHelp) {
+        require("./src/help");
+    } else if (command.command.startsWith("cd")) {
+        const directory = command.command.replace("cd ", "");
 
-    cd(directory);
-  } else if (command.command.startsWith("ls")) {
-    const directory = command.command.replace("ls ", "").startsWith("ls")
-      ? process.cwd()
-      : command.command.replace("ls ", "");
+        cd(directory);
+    } else if (command.command.startsWith("ls")) {
+        const directory = command.command.replace("ls ", "").startsWith("ls")
+            ? process.cwd()
+            : command.command.replace("ls ", "");
 
-    ls(directory);
-  } else if (command.command.startsWith("sysinfo")) {
-    const all = command.command.includes("-a");
+        ls(directory);
+    } else if (command.command.startsWith("sysinfo")) {
+        const all = command.command.includes("-a");
 
-    all ? sysinfo(true) : sysinfo();
-  } else if (command.command.startsWith("taskkill")) {
-    const pid = command.command.replace("taskkill ", "").startsWith("taskkill")
-      ? undefined
-      : command.command.replace("taskkill ", "");
+        all ? sysinfo(true) : sysinfo();
+    } else if (command.command.startsWith("taskkill")) {
+        const pid = command.command.replace("taskkill ", "").startsWith("taskkill")
+            ? undefined
+            : command.command.replace("taskkill ", "");
 
-    taskkill(pid);
-  }
+        taskkill(pid);
+    }
 }
--- variables/constants.js
+++ variables/constants.js
@@ -1,24 +1,36 @@
-const VERSION = "0.1.0";
+const VERSION = "0.1.5";
 
 const RECOGNIZED_COMMANDS = ["help", "exit", "cd", "sysinfo", "ls", "taskkill"];
 
 const DEFINITIONS = {
-  help: {
-    description: "Displays a list of available commands.",
-    usage: ["help", "help <command>"],
-  },
-  exit: {
-    description: "Exits the BubbleOS Lite shell.",
-    usage: ["exit"],
-  },
-  cd: {
-    description: "Changes the current working directory.",
-    usage: ["cd <directory>"],
-  },
+    help: {
+        description: "Displays a list of available commands.",
+        usage: ["help", "help <command>"],
+    },
+    exit: {
+        description: "Exits the BubbleOS Lite shell.",
+        usage: ["exit"],
+    },
+    cd: {
+        description: "Changes the current working directory.",
+        usage: ["cd <directory>"],
+    },
+    sysinfo: {
+        description: "Displays system information.",
+        usage: ["sysinfo"],
+    },
+    ls: {
+        description: "Displays the contents of the current working directory or other directories.",
+        usage: ["ls", "ls <directory>"],
+    },
+    taskkill: {
+        description: "Terminates a process using a PID.",
+        usage: ["taskkill <PID>"],
+    },
 };
 
 module.exports = {
-  VERSION,
-  RECOGNIZED_COMMANDS,
-  DEFINITIONS,
+    VERSION,
+    RECOGNIZED_COMMANDS,
+    DEFINITIONS,
 };
