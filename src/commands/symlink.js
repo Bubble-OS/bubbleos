94395521a4dc3039c1c429d9366400d4691ad74c
Arnav Thorat <62457196+arnavthorat78@users.noreply.github.com>
2023-03-06 02:23:23 +0000

    Made the code of `symlink` more concise

--- src/commands/symlink.js
+++ src/commands/symlink.js
@@ -1,56 +1,41 @@
 const chalk = require("chalk");
-
 const fs = require("fs");
 
-const Errors = require("../classes/Errors");
 const _convertAbsolute = require("../functions/convAbs");
+const _replaceSpaces = require("../functions/replaceSpaces");
 const _fatalError = require("../functions/fatalError");
 
-const _replaceSpaces = require("../functions/replaceSpaces");
+const Errors = require("../classes/Errors");
+const Checks = require("../classes/Checks");
+const { GLOBAL_NAME } = require("../variables/constants");
 
-const symlink = (path, newPath, ...params) => {
-  let check = false;
-  if (
-    params.includes("-c") ||
-    params.includes("/c") ||
-    newPath === "-c" ||
-    newPath === "/c" ||
-    newPath === "-c"
-  )
-    check = true;
-
-  if (typeof path === "undefined" || (!check && typeof newPath === "undefined")) {
-    Errors.enterParameter("a path/the paths", "symlink notSymbol symbol");
-    return;
-  }
-
-  path = _replaceSpaces(path);
-  path = _convertAbsolute(path);
-  if (!check) {
-    newPath = _replaceSpaces(newPath);
-    newPath = _convertAbsolute(newPath);
-  }
-
-  if (!check && !fs.existsSync(path)) {
-    Errors.doesNotExist("file/directory", path);
-    return;
-  }
-
+const symlink = (path, newPath, ...args) => {
   try {
-    if (check) {
-      const isSymlink = fs.lstatSync(path).isSymbolicLink();
+    const check =
+      args.includes("-c") || args.includes("/c") || newPath === "-c" || newPath === "/c";
 
-      if (isSymlink)
+    path = _convertAbsolute(_replaceSpaces(path));
+    if (!check) newPath = _convertAbsolute(_replaceSpaces(newPath));
+
+    const pathChk = new Checks(path);
+    const newPathChk = new Checks(newPath);
+
+    if (pathChk.paramUndefined() || newPathChk.paramUndefined()) {
+      Errors.enterParameter("a path/the paths", "symlink notSymbol symbol");
+      return;
+    }
+
+    if (!pathChk.doesExist()) {
+      Errors.doesNotExist("file/directory", path);
+      return;
+    }
+
+    if (check) {
+      if (fs.lstatSync(path).isSymbolicLink())
+        console.log(chalk.greenBright(`The path, ${chalk.italic(path)}, is a symbolic link.\n`));
+      else
         console.log(
-          chalk.greenBright(
-            `The path, ${chalk.italic(path)}, ${chalk.bold("is")} a symbolic link.\n`
-          )
-        );
-      else if (!isSymlink)
-        console.log(
-          chalk.hex("#FFA500")`The path, ${chalk.italic(path)}, ${chalk.bold(
-            "is not"
-          )} a symbolic link.\n`
+          chalk.hex("#FFA500")`The path, ${chalk.italic(path)}, is not a symbolic link.\n`
         );
 
       return;
@@ -58,14 +43,10 @@ const symlink = (path, newPath, ...params) => {
 
     fs.symlinkSync(path, newPath);
 
-    console.log(chalk.green("The operation completed successfully.\n"));
+    console.log(chalk.green(`Successfully linked ${chalk.bold(newPath)} to ${chalk.bold(path)}.`));
   } catch (err) {
     if (err.code === "EPERM") {
-      console.log(
-        chalk.yellow(
-          "Note: You may have to run this command with elevated privileges to make a symbolic link."
-        )
-      );
+      console.log(chalk.yellow(`Tip: Try running ${GLOBAL_NAME} with elevated privileges.`));
       Errors.noPermissions("make the symbolic link", newPath);
       return;
     } else {
