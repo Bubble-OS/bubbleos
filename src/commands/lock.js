327c256e32dfe8cb82754460528aebeb33484bb3
arnavthorat78 <arnavthorat78@gmail.com>
2023-03-24 16:24:11 +1100

    Completed the `lock` command

--- src/commands/lock.js
+++ src/commands/lock.js
@@ -1,14 +1,34 @@
 const chalk = require("chalk");
 const lockSystem = require("lock-system");
 
+const { GLOBAL_NAME } = require("../variables/constants");
+
+const _friendlyOS = require("../functions/friendlyOS");
 const _fatalError = require("../functions/fatalError");
 
-const lock = () => {
-  try {
-    console.log(chalk.yellow("WARNING: Very unstable command. Do not use right now! :)"));
+const Errors = require("../classes/Errors");
 
+const lock = (...args) => {
+  try {
     lockSystem();
+
+    console.log(chalk.green(`${GLOBAL_NAME} has successfully locked ${_friendlyOS()}!`));
+    if (Math.random() < 0.1) console.log(chalk.dim("We'll make sure no thieves get in! :D"));
+
+    console.log();
+    return;
   } catch (err) {
+    if (err.message.toLowerCase().includes("unsupported os")) {
+      Errors.invalidOS("Windows, macOS and/or Linux");
+      return;
+    } else if (err.message.toLowerCase().includes("no applicable command found")) {
+      console.log(
+        chalk.red(
+          "Locking the OS failed. Please consider installing xdg-screensaver, gnome-screensaver, cinnamon-screensaver, or dm-tool, and try again."
+        )
+      );
+      return;
+    }
     _fatalError(err);
   }
 };
